94990696c9342e00b88123670ed3a69877e03ec9
Alan <63525122+npcnumber0@users.noreply.github.com>
2024-05-27 12:39:56 -0400

    Minor Changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuToggle = document.querySelector('.menu-toggle');
-        const navi = document.querySelector('.navi');
+    const navi = document.querySelector('.navi');
+    menuToggle.addEventListener('click', () => {
+    navi.classList.toggle('active');
+});
 
-        menuToggle.addEventListener('click', () => {
-            navi.classList.toggle('active');
-        });
+
